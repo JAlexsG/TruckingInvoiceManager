3b0bdd525d01071dbb7c026bbecd7db66ddad240
Jonathan Alexander Castillo <jalexander.castillo7@gmail.com>
2024-10-31 14:50:14 -0400

    Small Fix to InvoiceTable.js

--- invoice-manager/src/components/InvoiceTable.js
+++ invoice-manager/src/components/InvoiceTable.js
@@ -1,3 +1,4 @@
+// src/components/InvoiceTable.js
 import React from 'react';
 
 const InvoiceTable = ({ invoices }) => {
@@ -15,6 +16,12 @@ const InvoiceTable = ({ invoices }) => {
                     <th>Invoice Date</th>
                     <th>Company Name</th>
                     <th>Company Address</th>
+                    <th>Company MC Number</th>
+                    <th>Company DOT Number</th>
+                    <th>Company FEIN</th>
+                    <th>Company City</th>
+                    <th>Company State</th>
+                    <th>Company ZIP</th>
                 </tr>
             </thead>
             <tbody>
@@ -30,6 +37,12 @@ const InvoiceTable = ({ invoices }) => {
                         <td>{invoice.invoice_date}</td>
                         <td>{invoice.company_name}</td>
                         <td>{invoice.company_address}</td>
+                        <td>{invoice.company_mc_number}</td>
+                        <td>{invoice.company_dot_number}</td>
+                        <td>{invoice.company_fein}</td>
+                        <td>{invoice.company_city}</td>
+                        <td>{invoice.company_state}</td>
+                        <td>{invoice.company_zip}</td>
                     </tr>
                 ))}
             </tbody>
